f2f22255ef61f7c45ef5cfcd323cc7e5ea1be491
jeremyolsen@msn.com <jeremyolsen@msn.com>
2019-02-14 14:01:13 -0800

    Code cleanup .  Removing unused cruft.

--- src/sampleFasterRCNN.cpp
+++ src/sampleFasterRCNN.cpp
@@ -61,12 +61,10 @@ static const int INPUT_C = 3;
 static const int INPUT_H = 375;
 static const int INPUT_W = 500;
 static const int IM_INFO_SIZE = 3;
-static const int OUTPUT_CLS_SIZE = 2;
+static const int OUTPUT_CLS_SIZE = 21;
 static const int OUTPUT_BBOX_SIZE = OUTPUT_CLS_SIZE * 4;
 
-//const std::string CLASSES[OUTPUT_CLS_SIZE]{ "background", "aeroplane", "bicycle", "bird", "boat", "bottle", "bus", "car", "cat", "chair", "cow", "diningtable", "dog", "horse", "motorbike", "person", "pottedplant", "sheep", "sofa", "train", "tvmonitor" };
-const std::string CLASSES[OUTPUT_CLS_SIZE]{ "background", "traffic_cone"};
-//const std::string CLASSES[OUTPUT_CLS_SIZE]{ "background", "tree"};
+const std::string CLASSES[OUTPUT_CLS_SIZE]{ "background", "aeroplane", "bicycle", "bird", "boat", "bottle", "bus", "car", "cat", "chair", "cow", "diningtable", "dog", "horse", "motorbike", "person", "pottedplant", "sheep", "sofa", "train", "tvmonitor" };
 
 const char* INPUT_BLOB_NAME0 = "data";
 const char* INPUT_BLOB_NAME1 = "im_info";
@@ -92,46 +90,6 @@ std::string locateFile(const std::string& input)
     return locateFile(input, dirs);
 }
 
-// simple PPM (portable pixel map) reader
-// void readPPMFile(const std::string& filename, PPM& ppm)
-// {
-// 	ppm.fileName = filename;
-// 	std::ifstream infile(locateFile(filename), std::ifstream::binary);
-// 	infile >> ppm.magic >> ppm.w >> ppm.h >> ppm.max;
-// 	infile.seekg(1, infile.cur);
-// 	infile.read(reinterpret_cast<char*>(ppm.buffer), ppm.w * ppm.h * 3);
-// }
-
-// void writePPMFileWithBBox(const std::string& filename, PPM& ppm, const BBox& bbox)
-// {
-// 	std::ofstream outfile("./" + filename, std::ofstream::binary);
-// 	assert(!outfile.fail());
-// 	outfile << "P6" << "\n" << ppm.w << " " << ppm.h << "\n" << ppm.max << "\n";
-// 	auto round = [](float x)->int {return int(std::floor(x + 0.5f)); };
-// 	for (int x = int(bbox.x1); x < int(bbox.x2); ++x)
-// 	{
-// 		// bbox top border
-// 		ppm.buffer[(round(bbox.y1) * ppm.w + x) * 3] = 255;
-// 		ppm.buffer[(round(bbox.y1) * ppm.w + x) * 3 + 1] = 0;
-// 		ppm.buffer[(round(bbox.y1) * ppm.w + x) * 3 + 2] = 0;
-// 		// bbox bottom border
-// 		ppm.buffer[(round(bbox.y2) * ppm.w + x) * 3] = 255;
-// 		ppm.buffer[(round(bbox.y2) * ppm.w + x) * 3 + 1] = 0;
-// 		ppm.buffer[(round(bbox.y2) * ppm.w + x) * 3 + 2] = 0;
-// 	}
-// 	for (int y = int(bbox.y1); y < int(bbox.y2); ++y)
-// 	{
-// 		// bbox left border
-// 		ppm.buffer[(y * ppm.w + round(bbox.x1)) * 3] = 255;
-// 		ppm.buffer[(y * ppm.w + round(bbox.x1)) * 3 + 1] = 0;
-// 		ppm.buffer[(y * ppm.w + round(bbox.x1)) * 3 + 2] = 0;
-// 		// bbox right border
-// 		ppm.buffer[(y * ppm.w + round(bbox.x2)) * 3] = 255;
-// 		ppm.buffer[(y * ppm.w + round(bbox.x2)) * 3 + 1] = 0;
-// 		ppm.buffer[(y * ppm.w + round(bbox.x2)) * 3 + 2] = 0;
-// 	}
-// 	outfile.write(reinterpret_cast<char*>(ppm.buffer), ppm.w * ppm.h * 3);
-// }
 
 void caffeToTRTModel(const std::string& deployFile,			// name for caffe prototxt
 	const std::string& modelFile,			// name for model
@@ -303,28 +261,7 @@ std::vector<int> nms(std::vector<std::pair<float, int> >& score_index, float* bb
 int main(int argc, char** argv)
 {
 	cv::CommandLineParser parser(argc, argv, params);
-    	
-    // cv::VideoCapture cap;
-    // if (parser.get<cv::String>("video").empty())
-    // {
-    //     //int cameraDevice = parser.get<int>("camera_device");
-    //     cap = cv::VideoCapture("nvcamerasrc ! video/x-raw(memory:NVMM), width=(int)640, height=(int)480,format=(string)I420, framerate=(fraction)30/1 ! nvvidconv ! video/x-raw, format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink");
-    //     if(!cap.isOpened())
-    //     {
-    //         cout << "Couldn't find camera: " << endl;
-    //         return -1;
-    //     }
-    // }
-    // else
-    // {
-    //     cap.open(parser.get<cv::String>("video"));
-    //     if(!cap.isOpened())
-    //     {
-    //         cout << "Couldn't open image or video: " << parser.get<cv::String>("video") << endl;
-    //         return -1;
-    //     }
-    // }
-
+    
 	sl::Camera zed;
     sl::InitParameters init_params;
     init_params.camera_resolution = sl::RESOLUTION_HD720;
@@ -345,8 +282,6 @@ int main(int argc, char** argv)
 
     // Prepare new image size to retrieve half-resolution images
     Resolution image_size = zed.getResolution();
-    // int new_width = image_size.width / 2;
-    // int new_height = image_size.height / 2;
 
     // To share data between sl::Mat and cv::Mat, use slMat2cvMat()
     // Only the headers and pointer to the sl::Mat are copied, not the data itself
@@ -358,37 +293,16 @@ int main(int argc, char** argv)
 	IHostMemory *trtModelStream{ nullptr };
 	// batch size
 	const int N = 1;
-	// caffeToTRTModel("faster_rcnn_test_iplugin.prototxt",
-	// 	"almonds_vgg16_faster_rcnn_iter_20000.caffemodel",
-	// 	std::vector < std::string > { OUTPUT_BLOB_NAME0, OUTPUT_BLOB_NAME1, OUTPUT_BLOB_NAME2 },
-	// 	N, &pluginFactorySerialize, &trtModelStream);
 	caffeToTRTModel("faster_rcnn_test_iplugin.prototxt",
-		"vgg16_faster_rcnn_iter_20000.caffemodel",
+		"VGG16_faster_rcnn_final.caffemodel",
 		std::vector < std::string > { OUTPUT_BLOB_NAME0, OUTPUT_BLOB_NAME1, OUTPUT_BLOB_NAME2 },
 		N, &pluginFactorySerialize, &trtModelStream);
     assert(trtModelStream != nullptr);
 	pluginFactorySerialize.destroyPlugin();
 
-/*
-	// read a random sample image
-	srand(unsigned(time(nullptr)));
-	// available images
-	std::vector<std::string> imageList = { "IMG_1551.ppm", "IMG_1553.ppm", "IMG_1556.ppm", "IMG_1559.ppm" };
-	//std::vector<std::string> imageList = { "IMG_1559.ppm" };
-	std::vector<PPM> ppms(N);
-
-	
-	std::random_shuffle(imageList.begin(), imageList.end(), [](int i) {return rand() % i; });
-	assert(ppms.size() <= imageList.size());
-*/
-
 	float imInfo[N * 3]; // input im_info
 	for (int i = 0; i < N; ++i)
 	{
-		// readPPMFile(imageList[i], ppms[i]);
-		// imInfo[i * 3] = float(ppms[i].h);   // number of rows
-		// imInfo[i * 3 + 1] = float(ppms[i].w); // number of columns
-		// imInfo[i * 3 + 2] = 1;         // image scale
 		imInfo[i * 3] 	  = float(INPUT_H);	//float(ppms[i].h); 	// number of rows
 		imInfo[i * 3 + 1] = float(INPUT_W);	//float(ppms[i].w); 	// number of columns
 		imInfo[i * 3 + 2] = 1;         		// image scale
@@ -418,143 +332,119 @@ int main(int argc, char** argv)
 
 	float img_scale_ratio_w, img_scale_ratio_h;
 
-	
-	
     char key = ' ';
     while (key != 'q')
-    //for (int m=0; m<1; m++)
     {
         if (zed.grab(runtime_parameters) == SUCCESS) {
 			// std::cout << "zed.grab - success" << std::endl;
 			zed.retrieveImage(image_zed, VIEW_LEFT, MEM_CPU, image_size.width, image_size.height);
 		
-        //cap >> frame; // get a new frame from camera/video or read image
 
-        if (image_ocv.empty())
-        {
-            cv::waitKey(1);
-            break;
-        }
-		
-		// std::cout << "image channels: " << image_ocv.channels() << std::endl;
-		// if (image_ocv.channels() == 4) {
-		// 	std::cout << "Converting Color" << std::endl;	
-		// 	cv::cvtColor(image_ocv, image_ocv, cv::COLOR_BGRA2BGR);
-		// }
-		// std::cout << "OpenCV captured img size[ : " << image_ocv.size().height <<"x"<<image_ocv.size().width<<"]"<<std::endl; 
-
-		// scale to size required by engine - INPUT_H*INPUT_W
-		cv::Size size(INPUT_W,INPUT_H);	//the dst image size,e.g.100x100
-		cv::Mat image_ocv_scaled;	
-		cv::resize(image_ocv,image_ocv_scaled,size);	//resize image from src->dst
-		// std::cout << "OpenCV scaled img size[ : " << image_ocv_scaled.size().height <<"x"<<image_ocv_scaled.size().width<<"]"<<std::endl;	
-
-		img_scale_ratio_w=float(image_ocv.size().width)/float(image_ocv_scaled.size().width);	//beaware the int/int rounding 
-		img_scale_ratio_h=float(image_ocv.size().height)/float(image_ocv_scaled.size().height);
-		cv::Mat_<cv::Vec3f>::iterator it;
-		unsigned volChl = INPUT_H*INPUT_W;
-		for (int c = 0; c < INPUT_C; ++c)                              
-		{
-			cv::Mat_<cv::Vec3b>::iterator it = image_ocv_scaled.begin<cv::Vec3b>();	//cv::Vec3f not working - reason still unknown...
-			// the color image to input should be in BGR order
-			for (unsigned j = 0; j < volChl; ++j)
+			if (image_ocv.empty())
 			{
-				//OpenCV read in image_ocv as BGR format, by default, thus need only deduct the mean value
-				data[c*volChl + j] = float((*it)[c]) - pixelMean[c];
-				it++;
+				cv::waitKey(1);
+				break;
 			}
-		}
-	// run inference
-	doInference(*context, data, imInfo, bboxPreds, clsProbs, rois, N);
+			
+			cv::Size size(INPUT_W,INPUT_H);	//the dst image size,e.g.100x100
+			cv::Mat image_ocv_scaled;	
+			cv::resize(image_ocv,image_ocv_scaled,size);	//resize image from src->dst
+			// std::cout << "OpenCV scaled img size[ : " << image_ocv_scaled.size().height <<"x"<<image_ocv_scaled.size().width<<"]"<<std::endl;	
 
-	// unscale back to raw image space
-	for (int i = 0; i < N; ++i)
-	{
-		float * rois_offset = rois + i * nmsMaxOut * 4;
-		for (int j = 0; j < nmsMaxOut * 4 && imInfo[i * 3 + 2] != 1; ++j)
-			rois_offset[j] /= imInfo[i * 3 + 2];
-	}
-	bboxTransformInvAndClip(rois, bboxPreds, predBBoxes, imInfo, N, nmsMaxOut, OUTPUT_CLS_SIZE);
-
-	const float nms_threshold = 0.3f;
-	const float score_threshold = 0.8f;
-
-	for (int i = 0; i < N; ++i)
-	{
-		float *bbox = predBBoxes + i * nmsMaxOut * OUTPUT_BBOX_SIZE;
-		float *scores = clsProbs + i * nmsMaxOut * OUTPUT_CLS_SIZE;
-		for (int c = 1; c < OUTPUT_CLS_SIZE; ++c) // skip the background
-		{
-			std::vector<std::pair<float, int> > score_index;
-			for (int r = 0; r < nmsMaxOut; ++r)
+			img_scale_ratio_w=float(image_ocv.size().width)/float(image_ocv_scaled.size().width);	//beaware the int/int rounding 
+			img_scale_ratio_h=float(image_ocv.size().height)/float(image_ocv_scaled.size().height);
+			cv::Mat_<cv::Vec3f>::iterator it;
+			unsigned volChl = INPUT_H*INPUT_W;
+			for (int c = 0; c < INPUT_C; ++c)                              
 			{
-				if (scores[r*OUTPUT_CLS_SIZE + c] > score_threshold)
+				cv::Mat_<cv::Vec3b>::iterator it = image_ocv_scaled.begin<cv::Vec3b>();	//cv::Vec3f not working - reason still unknown...
+				// the color image to input should be in BGR order
+				for (unsigned j = 0; j < volChl; ++j)
 				{
-					score_index.push_back(std::make_pair(scores[r*OUTPUT_CLS_SIZE + c], r));
-					std::stable_sort(score_index.begin(), score_index.end(),
-						[](const std::pair<float, int>& pair1,
-							const std::pair<float, int>& pair2) 
-					{
-						return pair1.first > pair2.first;
-					});
+					//OpenCV read in image_ocv as BGR format, by default, thus need only deduct the mean value
+					data[c*volChl + j] = float((*it)[c]) - pixelMean[c];
+					it++;
 				}
 			}
-			// apply NMS algorithm
-			std::vector<int> indices = nms(score_index, bbox, c, OUTPUT_CLS_SIZE, nms_threshold);
+			// run inference
+			doInference(*context, data, imInfo, bboxPreds, clsProbs, rois, N);
 
-			// Show results
-			// if (indices.size()>0)
-			// 	std::cout<<"size of index: "<<indices.size()<<std::endl;
-
-			for (unsigned k = 0; k < indices.size(); ++k)
+			// unscale back to raw image space
+			for (int i = 0; i < N; ++i)
 			{
-				int idx = indices[k];
-				// std::string storeName = CLASSES[c] + "-" + std::to_string(scores[idx*OUTPUT_CLS_SIZE + c]) + ".ppm";
-				// std::cout << "Detected " << CLASSES[c] << " in " << ppms[i].fileName << " with confidence " << scores[idx*OUTPUT_CLS_SIZE + c] * 100.0f << "% "
-				// 	<< " (Result stored in " << storeName << ")." << std::endl;
-
-				//BBox b{ bbox[idx*OUTPUT_BBOX_SIZE + c * 4], bbox[idx*OUTPUT_BBOX_SIZE + c * 4 + 1], bbox[idx*OUTPUT_BBOX_SIZE + c * 4 + 2], bbox[idx*OUTPUT_BBOX_SIZE + c * 4 + 3] };
-				//writePPMFileWithBBox(storeName, ppms[i], b);
-
-				int xLeftBottom = bbox[idx*OUTPUT_BBOX_SIZE + c * 4];
-				int yLeftBottom = bbox[idx*OUTPUT_BBOX_SIZE + c * 4 + 1];
-				int xRightTop 	= bbox[idx*OUTPUT_BBOX_SIZE + c * 4 + 2];
-				int yRightTop	= bbox[idx*OUTPUT_BBOX_SIZE + c * 4 + 3];
-
-				//cv::Rect object0((int)xLeftBottom, (int)yLeftBottom, (int)(xRightTop - xLeftBottom), (int)(yRightTop - yLeftBottom));
-	                	//rectangle(image_ocv_scaled, object0, cv::Scalar(0, 255, 0));				
-	                									
-				//convert vertexes back into unscaled image
-				xLeftBottom = (int) (xLeftBottom * img_scale_ratio_w); 
-				yLeftBottom = (int) (yLeftBottom * img_scale_ratio_h);
-				xRightTop   = (int) (xRightTop * img_scale_ratio_w); 
-				yRightTop   = (int) (yRightTop * img_scale_ratio_h);
-				
-				//draw bbox
-				cv::Rect object((int)xLeftBottom, (int)yLeftBottom, (int)(xRightTop - xLeftBottom), (int)(yRightTop - yLeftBottom));
-	                	rectangle(image_ocv, object, cv::Scalar(0, 255, 0));
-	                	
-	                	//draw cls tags
-	                	ostringstream ss("");
-	                	//ss.str("");
-                		ss << scores[idx*OUTPUT_CLS_SIZE + c] * 100.0f;
-		                cv::String str_conf(ss.str());
-                		cv::String label = cv::String(CLASSES[c]) + ": " + str_conf;
-                		int baseLine = 0;
-		                cv::Size labelSize = cv::getTextSize(label, cv::FONT_HERSHEY_SIMPLEX, 0.5, 1, &baseLine);
-                		cv::rectangle(image_ocv, cv::Rect(cv::Point(xLeftBottom, yLeftBottom - labelSize.height), cv::Size(labelSize.width, labelSize.height + baseLine)),
-                          	cv::Scalar(255,128,255), CV_FILLED);
-                		cv::putText(image_ocv, label, cv::Point(xLeftBottom, yLeftBottom), cv::FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(0,0,0));				
+				float * rois_offset = rois + i * nmsMaxOut * 4;
+				for (int j = 0; j < nmsMaxOut * 4 && imInfo[i * 3 + 2] != 1; ++j)
+					rois_offset[j] /= imInfo[i * 3 + 2];
 			}
+			bboxTransformInvAndClip(rois, bboxPreds, predBBoxes, imInfo, N, nmsMaxOut, OUTPUT_CLS_SIZE);
 
-		}
-		}
-	}
+			const float nms_threshold = 0.3f;
+			const float score_threshold = 0.8f;
 
-	//update result in opencv window
+			for (int i = 0; i < N; ++i)
+			{
+				float *bbox = predBBoxes + i * nmsMaxOut * OUTPUT_BBOX_SIZE;
+				float *scores = clsProbs + i * nmsMaxOut * OUTPUT_CLS_SIZE;
+				for (int c = 1; c < OUTPUT_CLS_SIZE; ++c) // skip the background
+				{
+					std::vector<std::pair<float, int> > score_index;
+					for (int r = 0; r < nmsMaxOut; ++r)
+					{
+						if (scores[r*OUTPUT_CLS_SIZE + c] > score_threshold)
+						{
+							score_index.push_back(std::make_pair(scores[r*OUTPUT_CLS_SIZE + c], r));
+							std::stable_sort(score_index.begin(), score_index.end(),
+								[](const std::pair<float, int>& pair1,
+									const std::pair<float, int>& pair2) 
+							{
+								return pair1.first > pair2.first;
+							});
+						}
+					}
+					// apply NMS algorithm
+					std::vector<int> indices = nms(score_index, bbox, c, OUTPUT_CLS_SIZE, nms_threshold);
+
+
+					for (unsigned k = 0; k < indices.size(); ++k)
+					{
+						int idx = indices[k];
+						// std::string storeName = CLASSES[c] + "-" + std::to_string(scores[idx*OUTPUT_CLS_SIZE + c]) + ".ppm";
+						// std::cout << "Detected " << CLASSES[c] << " in " << ppms[i].fileName << " with confidence " << scores[idx*OUTPUT_CLS_SIZE + c] * 100.0f << "% "
+						// 	<< " (Result stored in " << storeName << ")." << std::endl;
+
+						int xLeftBottom = bbox[idx*OUTPUT_BBOX_SIZE + c * 4];
+						int yLeftBottom = bbox[idx*OUTPUT_BBOX_SIZE + c * 4 + 1];
+						int xRightTop 	= bbox[idx*OUTPUT_BBOX_SIZE + c * 4 + 2];
+						int yRightTop	= bbox[idx*OUTPUT_BBOX_SIZE + c * 4 + 3];			
+																
+						//convert vertexes back into unscaled image
+						xLeftBottom = (int) (xLeftBottom * img_scale_ratio_w); 
+						yLeftBottom = (int) (yLeftBottom * img_scale_ratio_h);
+						xRightTop   = (int) (xRightTop * img_scale_ratio_w); 
+						yRightTop   = (int) (yRightTop * img_scale_ratio_h);
+						
+						//draw bbox
+						cv::Rect object((int)xLeftBottom, (int)yLeftBottom, (int)(xRightTop - xLeftBottom), (int)(yRightTop - yLeftBottom));
+								rectangle(image_ocv, object, cv::Scalar(0, 255, 0));
+								
+								//draw cls tags
+								ostringstream ss("");
+								//ss.str("");
+								ss << scores[idx*OUTPUT_CLS_SIZE + c] * 100.0f;
+								cv::String str_conf(ss.str());
+								cv::String label = cv::String(CLASSES[c]) + ": " + str_conf;
+								int baseLine = 0;
+								cv::Size labelSize = cv::getTextSize(label, cv::FONT_HERSHEY_SIMPLEX, 0.5, 1, &baseLine);
+								cv::rectangle(image_ocv, cv::Rect(cv::Point(xLeftBottom, yLeftBottom - labelSize.height), cv::Size(labelSize.width, labelSize.height + baseLine)),
+									cv::Scalar(255,128,255), CV_FILLED);
+								cv::putText(image_ocv, label, cv::Point(xLeftBottom, yLeftBottom), cv::FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(0,0,0));				
+					}
+				}
+			}
+		}
+
+		//update result in opencv window
         cv::imshow("detections1", image_ocv);
-        //cv::imshow("detections2", image_ocv_scaled);
-        //if (cv::waitKey(3) >= 0) break;	//1ms
 		key = cv::waitKey(10);
     }
 
